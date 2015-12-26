60f60571f889b0aead212c374d2a5d53de39f105
bruno2546 <bruno9pereira@gmail.com>
2015-12-26 17:27:44 -0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Warehouse - Finder</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="assets/css/main.css" type="text/css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+	<h1 class="title-text">Warehouse <small>Finder</small></div>	
+
+
+</body>
+</html>
