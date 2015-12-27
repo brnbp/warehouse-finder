038e056cfaf64efe362b64d735fafc21191833d1
bruno2546 <bruno9pereira@gmail.com>
2015-12-27 00:56:44 -0200

    New fields

--- index.php
+++ index.php
@@ -42,8 +42,8 @@
 
 				<div class="row">
 					<div class="form-group col-xs-7 col-md-3 col-sm-4">
-						<select name="level" id="level" multiple class="form-control">
-							<option disabled> Choose Level</option>
+						<select name="level" id="level" class="form-control">
+							<option disabled selected> Choose Level</option>
 							<option value="info">Info</option>
 							<option value="warning">Warning</option>
 							<option value="critical">Critical</option>
@@ -53,33 +53,56 @@
 
 				<div class="row">
 					<div class="form-group col-xs-7 col-md-3 col-sm-4">	
-						<input type="text" id="logname" placeholder="Log Name" id="" class="form-control">
+						<input type="text" id="logname" placeholder="Log Name" class="form-control">
 					</div>
 				</div>
 
 				<div class="row">
-					<div class="form-group col-xs-7 col-md-3 col-sm-4">	
+					<div class="form-group col-xs-7 col-md-3 col-sm-4">
 						<input type="text" id="identifier" placeholder="Identifier"  class="form-control">
 					</div>
-				</div>	
-				
+				</div>
+
 				<div class="row">
+					<div class="form-group col-xs-7 col-md-3 col-sm-4">
+						<input type="number" id="limit" placeholder="Return limit (default 15)"  class="form-control" min="1" max="400">
+					</div>
+				</div>
+				
+				<!--div class="row">
 					<div class="form-group col-xs-7 col-md-3 col-sm-4">	
 						<input type="text" id="message" placeholder="Message part" class="form-control">
 					</div>
-				</div>
+				</div-->
 				
 				<div class="row">
-					<div class="form-group col-xs-2 col-md-1 col-sm-1"></div> 
+					<div class="form-group col-xs-2 col-md-1 col-sm-1"></div>
 						<button class="glyphicon glyphicon-search btn btn-default" id="search" type="button"> Search!</button>
+						<button class="glyphicon glyphicon-erase btn btn-default" id="clear-request" type="button" disabled> Clear</button>
 					</div>
 				</div>
-				
-				
 			</form>
 		</div>
 
 		<hr>
+
+		<div class="container">
+			<table class="table table-responsive table-hover" id="logs" hidden>
+				<thead>
+					<tr>
+						<td>#</td>
+						<td>created at</td>
+						<td>updated in</td>
+						<td>incidents</td>
+						<td>identifier</td>
+						<td>log name</td>
+						<td>messages</td>
+					</tr>
+				</thead>
+				<tbody class="table-row">
+				</tbody>
+			</table>
+		</div>
 	</div>
 </body>
 </html>
